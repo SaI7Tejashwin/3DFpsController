f8f291e3d01f5e9725818f7c37fc1bc87fdc4926
SaI7Tejashwin <57253627+SaI7Tejashwin@users.noreply.github.com>
2020-04-24 06:15:23 +0530

    Added files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public CharacterController characterController;
+    public float speed = 12f;
+
+    private Vector3 velocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 2f;
+    public LayerMask groundMask;
+    [SerializeField] private float gravity = -9.81f;
+
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontalInput + transform.forward * verticalInput;
+
+        characterController.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) //or Input.GetKeyDown("Jump")
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+
+    }
+}
